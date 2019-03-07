3ddd3d0b0c22e6cfd7642ae8e03fecc9b5fff1b5
Ahnder <skguss1@gmail.com>
2019-03-07 21:06:38 +0900

    initial commit

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package Calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("더하기 : " + (3 + 4));
+		System.out.println("뺴기 : " + (6 - 3));
+		System.out.println("곱하기 : " + (3 * 4));
+		System.out.println("나누기 : " + (8 + 4));
+
+	}
+
+}
